30ebf8ab5539e6104ebe3b489e16ec1057cc63f5
nadgirabhishek <Abhishek.Nadgir@colorado.edu>
2025-09-26 12:42:45 -0600

    newtry

--- src/main.c
+++ src/main.c
@@ -4,35 +4,37 @@
 #define LED_PIN 5
 #define SYSTEM_CORE_CLOCK 8000000UL
 
-volatile uint32_t g_ms = 0;
-
-/* Mark as used so --gc-sections never drops it, and read CTRL to clear COUNTFLAG */
-void SysTick_Handler(void) __attribute__((used));
-void SysTick_Handler(void)
+static inline void systick_init_1ms(void)
 {
-    g_ms++;
-    (void)SYST->CTRL;   // read to clear COUNTFLAG
+    SYST->LOAD = (SYSTEM_CORE_CLOCK / 1000) - 1;  // 1 ms reload
+    SYST->VAL  = 0;
+    // Enable counter, use core clock, BUT DO NOT enable interrupt
+    SYST->CTRL = SYST_CTRL_CLKSRC | SYST_CTRL_ENABLE;
 }
 
-static void delay_ms(uint32_t ms) {
-    uint32_t start = g_ms;
-    while ((g_ms - start) < ms) { __asm volatile ("nop"); }
+static void delay_ms(uint32_t ms)
+{
+    while (ms--)
+    {
+        // COUNTFLAG is bit 16; it is set to 1 when the counter wraps
+        // Reading CTRL returns it; next wrap will set it again.
+        while ((SYST->CTRL & (1u << 16)) == 0) { __asm volatile("nop"); }
+    }
 }
 
-int main(void) {
-    /* enable GPIOA clock */
+int main(void)
+{
+    // Enable GPIOA clock
     RCC->AHBENR |= (1u << 17);
 
-    /* PA5 as output */
-    GPIOA->MODER &= ~(3u << (LED_PIN*2));
-    GPIOA->MODER |=  (1u << (LED_PIN*2));
+    // PA5 output
+    GPIOA->MODER &= ~(3u << (LED_PIN * 2));
+    GPIOA->MODER |=  (1u << (LED_PIN * 2));
 
-    /* SysTick 1ms tick @ 8 MHz */
-    SYST->LOAD = (SYSTEM_CORE_CLOCK / 1000) - 1;
-    SYST->VAL  = 0;
-    SYST->CTRL = SYST_CTRL_CLKSRC | SYST_CTRL_TICKINT | SYST_CTRL_ENABLE;
+    systick_init_1ms();
 
-    while (1) {
+    for (;;)
+    {
         GPIOA->ODR ^= (1u << LED_PIN);
         delay_ms(500);
     }
