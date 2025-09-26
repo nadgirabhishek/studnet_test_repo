b0f89924b5e0012a46a1deaebc2c161580d9d669
nadgirabhishek <Abhishek.Nadgir@colorado.edu>
2025-09-26 12:06:07 -0600

    temp

--- startup_stm32f0.s
+++ startup_stm32f0.s
@@ -1,10 +1,15 @@
-/* Minimal STM32F0 (Cortex-M0) startup file with correct SysTick override and VTOR set */
+/* STM32F0 (Cortex-M0) startup:
+ * - No multi-instruction IT blocks (ARMv6-M)
+ * - No post-indexed LDR/STR (Thumb-1)
+ * - Do NOT alias SysTick_Handler; C override will work
+ * - No VTOR write (not on M0); vector table must be first in FLASH
+ */
 
 .syntax unified
 .cpu cortex-m0
 .thumb
 
-/* External symbols provided by the linker script */
+/* Linker-provided symbols */
 .extern _sidata
 .extern _sdata
 .extern _edata
@@ -13,16 +18,14 @@
 .extern _estack
 .extern main
 
-/*----------------------------------------------------------------------------
- * Vector table
- *---------------------------------------------------------------------------*/
+/* ---------------- Vector Table ---------------- */
 .section .isr_vector, "a", %progbits
 .align  2
 .global g_pfnVectors
 .type   g_pfnVectors, %object
 
 g_pfnVectors:
-  .word  _estack            /* Initial Stack Pointer */
+  .word  _estack            /* Initial SP */
   .word  Reset_Handler      /* Reset */
   .word  NMI_Handler        /* NMI */
   .word  HardFault_Handler  /* HardFault */
@@ -39,84 +42,67 @@ g_pfnVectors:
   .word  PendSV_Handler     /* PendSV */
   .word  SysTick_Handler    /* SysTick (IRQ 15) */
 
-/* If you have device-specific IRQs, list them here after SysTick.
- * For a basic template, we leave them out or alias them in Default_Handler.
- */
-
-/*----------------------------------------------------------------------------
- * Default weak handlers
- *---------------------------------------------------------------------------*/
 .text
 .thumb
 .align 1
 
-/* Declare handlers weak so user code can override them */
+/* ---------------- Weak Handlers ---------------- */
 .weak  NMI_Handler
 .weak  HardFault_Handler
 .weak  SVC_Handler
 .weak  PendSV_Handler
 .weak  SysTick_Handler
 
-/* All but SysTick get aliased to Default_Handler.
- * DO NOT alias SysTick_Handler, so a C definition overrides properly.
- */
 .thumb_set NMI_Handler,       Default_Handler
 .thumb_set HardFault_Handler, Default_Handler
 .thumb_set SVC_Handler,       Default_Handler
 .thumb_set PendSV_Handler,    Default_Handler
-/* No alias for SysTick_Handler on purpose */
+/* DO NOT alias SysTick_Handler */
 
-/*----------------------------------------------------------------------------
- * Reset handler: init data/bss, set VTOR, enable IRQs, call main
- *---------------------------------------------------------------------------*/
+/* ---------------- Reset Handler ---------------- */
 .global Reset_Handler
 .type   Reset_Handler, %function
 Reset_Handler:
-  /* Copy .data from FLASH to RAM */
-  ldr   r0, =_sidata
-  ldr   r1, =_sdata
-  ldr   r2, =_edata
-1:
+  /* Copy .data from FLASH (_sidata) to RAM (_sdata .. _edata) */
+  ldr   r0, =_sidata        /* src */
+  ldr   r1, =_sdata         /* dst */
+  ldr   r2, =_edata         /* end */
+copy_data_loop:
   cmp   r1, r2
-  ittt  lt
-  ldrlt r3, [r0], #4
-  strlt r3, [r1], #4
-  blt   1b
+  bcs   copy_data_done
+  ldr   r3, [r0]
+  str   r3, [r1]
+  adds  r0, r0, #4
+  adds  r1, r1, #4
+  b     copy_data_loop
+copy_data_done:
 
-  /* Zero .bss */
-  ldr   r0, =_sbss
-  ldr   r1, =_ebss
+  /* Zero .bss (_sbss .. _ebss) */
+  ldr   r0, =_sbss          /* dst */
+  ldr   r1, =_ebss          /* end */
   movs  r2, #0
-2:
+zero_bss_loop:
   cmp   r0, r1
-  it    lt
-  strlt r2, [r0], #4
-  blt   2b
+  bcs   zero_bss_done
+  str   r2, [r0]
+  adds  r0, r0, #4
+  b     zero_bss_loop
+zero_bss_done:
 
-  /* Point VTOR at our vector table (SCB->VTOR = &g_pfnVectors) */
-  ldr   r0, =g_pfnVectors
-  ldr   r1, =0xE000ED08   /* SCB->VTOR */
-  str   r0, [r1]
-
-  /* Enable IRQs just in case (should be enabled by default) */
+  /* Enable IRQs (usually already enabled) */
   cpsie i
 
-  /* Call main */
+  /* Jump to main */
   bl    main
 
-  /* If main returns, loop forever */
-3:
-  b     3b
+hang:
+  b     hang
 
 .size Reset_Handler, .-Reset_Handler
 
-/*----------------------------------------------------------------------------
- * Default handler
- *---------------------------------------------------------------------------*/
+/* ---------------- Default Handler ---------------- */
 .global Default_Handler
 .type   Default_Handler, %function
 Default_Handler:
-  /* Stay here if an unexpected interrupt occurs */
   b Default_Handler
-
 .size Default_Handler, .-Default_Handler
